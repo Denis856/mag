c8531b5eb10af19f1678f3373707342175c451f1
Den89 <malyhindenis8@gmail.com>
2023-02-11 19:07:52 +0500

    ool

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ function showImg(e) {
     if(e > img.length) {indexValue = 1;}
     if(e < 1) {indexValue = img.length;}
     for(i = 0; i < img.length; i++) {
-        img[i].style.opacity = "0";
+        img[i].style.display = "none";
     }
-    img[indexValue - 1].style.opacity = "1";
+    img[indexValue - 1].style.display = "block";
 }
 
 // next
